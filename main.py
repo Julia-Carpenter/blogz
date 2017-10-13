f0b9f426e290ef92ef9d4c318c6c8a6ca20ca96c
Julia Carpenter <JCarpenter928@gmail.com>
2017-10-12 20:29:17 -0500

    Started to change it to Blog instead of Task.

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Julia@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
+class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    title = db.Column(db.String(400))
+    body = db.Column(db.String(10000))
 
     def __init__(self, name):
-        self.name = name
-        self.completed = False
+        self.title = title
+        self.body = body
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -24,11 +24,11 @@ def index():
 
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)
+        new_task = Blog(task_name)
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
+    tasks = Blog.query.filter_by(completed=False).all()
     completed_tasks = Task.query.filter_by(completed=True).all()
     return render_template('todos.html',title="Get It Done!", 
         tasks=tasks, completed_tasks=completed_tasks)
@@ -38,7 +38,7 @@ def index():
 def delete_task():
 
     task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
+    task = Blog.query.get(task_id)
     task.completed = True
     db.session.add(task)
     db.session.commit()

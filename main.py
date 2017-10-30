445908380fc1dbb5cc4c83c48b021ef601b22844
Julia Carpenter <JCarpenter928@gmail.com>
2017-10-29 22:01:44 -0500

    I cant check my work but I did part one.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Julia@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:Julia@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y337kGcys&zP3B'
@@ -14,10 +14,24 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(400))
     body = db.Column(db.String(10000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 @app.route('/blog', methods=['POST', 'GET'])
@@ -38,11 +52,14 @@ def index():
 def create_new_entry():
     headline = ""
     body = ""
+    user = ""
     if request.method == 'POST':
         headline = request.form['headline']
         body = request.form['body']
+        #TODO the owner line below this might be wrong
+        owner = User.query.filter_by(username=session['username']).first()
         if headline and body:
-            new_entry = Blog(headline, body)
+            new_entry = Blog(headline, body, owner)
             db.session.add(new_entry)
             db.session.commit()
             thisentry = Blog.query.order_by('-id').first()
